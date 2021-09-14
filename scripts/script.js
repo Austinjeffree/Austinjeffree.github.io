7bdeef65a29086d5e11cdc65f6ac898b76038dd6
AustinJeffree <austinjeffree@gmail.com>
2021-09-14 18:01:13 -0500

    java, styling, and HTML for modal box on products page

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,24 @@
 /* navigation for tabs in product page */
 function browseProducts(evt, productCategory) {
-    var i, availableProducts, tablinks; 
+    var i, availableProducts; 
     availableProducts = document.getElementsByClassName("availableProducts");
     for (i = 0; i < availableProducts.length; i++) {
       availableProducts[i].style.display = "none";
     }
 
-    tablinks = document.getElementsByClassName("tablinks");
-
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
     document.getElementById(productCategory).style.display = "grid";
     evt.currentTarget.className += " active";
   }
+
+
+var close = document.getElementsByClassName("close");
+ 
+function openProduct(productInfo) {
+    document.getElementById(productInfo).style.display = "block";
+
+}
+
+function closeProduct(productInfo) {;
+    document.getElementById(productInfo).style.display = "none";
+    
+}
